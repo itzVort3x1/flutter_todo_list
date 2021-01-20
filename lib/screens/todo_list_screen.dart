97b63d501b42005d7eec01acb19d9f582d9b9264
Kaustubh <saikaustubh10@gmail.com>
2021-01-20 17:58:13 +0530

    added ListView.builder

---
+++ lib/screens/todo_list_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class TodoListScreen extends StatelessWidget {
+
+  Widget _buildTask(int index, context){
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('Task title'),
+            subtitle: Text('Oct 2, 2019 * High'),
+            trailing: Checkbox(onChanged: (value){
+              print(value);
+              },
+              activeColor: Theme.of(context).primaryColor,
+              value: true,
+            ),
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(backgroundColor: Theme.of(context).primaryColor,
+      child: Icon(Icons.add),
+        onPressed: (){},
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 80.0),
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index){
+          if (index == 0){
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('My Tasks',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                  SizedBox(height: 10.0,),
+                  Text('1 of 10',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),),
+                ],
+              ),
+            );
+          }
+          return _buildTask(index, context);
+        },
+      ),
+    );
+  }
+}
